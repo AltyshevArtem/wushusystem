6944c5a65c68c953acc13cf9852adb465c016e3d
duutka <61553665+duutka@users.noreply.github.com>
2021-05-25 01:00:17 +0500

    Создали серверный проект + инициализировали модель  (#1)
    * примерная модель
    
    * create_new_app + model
    
    * Доработки модели
    
    * повысил регистры
    
    * some_changes
    
    * +gitignore and requirements
    
    * Наши доработки по модели
    
    * app sporstsman + models
    
    * delete useless gitignore
    
    Co-authored-by: Baraev Sergey <Baraev.sergey@mail.ru>

--- server/wushusystemserver/wushusystemserver/settings.py
+++ server/wushusystemserver/wushusystemserver/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sportsmans',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
