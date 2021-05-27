f9df74692cdb96da66d07763b84143df74c2c9e5
duutka <loj7mlbdf6l9@gmail.com>
2021-05-28 00:13:36 +0500

    fix eslint error

--- client/wushusystemclient/.eslintrc.js
+++ client/wushusystemclient/.eslintrc.js
@@ -1,31 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        mocha: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'max-len': ['error', { code: 100 }],
+        'prefer-promise-reject-errors': ['off'],
+        'no-return-assign': ['off'],
+        camelcase: 'warn',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                mocha: true,
+            },
+        },
+    ],
 };
--- client/wushusystemclient/prettier.config.js
+++ client/wushusystemclient/prettier.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     printWidth: 100,
-    trailingComma: "true",
+    trailingComma: 'all',
     tabWidth: 4,
     semi: true,
     singleQuote: true,
-    endOfLine: "auto",
 };
