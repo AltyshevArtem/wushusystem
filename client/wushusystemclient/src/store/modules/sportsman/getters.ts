e17717c27b66d186b7ea6ce3d36cb6c8715d8a16
altyshevartem <altyshev2013@mail.ru>
2021-06-15 23:19:40 +0500

    создание карточки спортсмена

--- client/wushusystemclient/src/store/modules/sportsman/getters.ts
+++ client/wushusystemclient/src/store/modules/sportsman/getters.ts
@@ -10,4 +10,52 @@ export const getters: GetterTree<ISportsmanState, null> = {
         const patronymic = (sportsman && sportsman.patronymic) || '';
         return `${surname} ${name} ${patronymic}`;
     },
+    Gender(state): string {
+        const { sportsman } = state;
+        const gender = (sportsman && sportsman.gender) || '';
+        return `${gender}`;
+    },
+    SportsmanPhoto(state): string {
+        const { sportsman } = state;
+        const src = (sportsman && sportsman.photo) || '';
+        // const sportsman_photo = (sportsman && sportsman.photo) || '';
+        return `${src}`;
+    },
+    DateOfBirth(state): string {
+        const { sportsman } = state;
+        const date_of_birth = (sportsman && sportsman.date_of_birth) || '';
+        return `${date_of_birth}`;
+    },
+    Address(state): string {
+        const { sportsman } = state;
+        const address = (sportsman && sportsman.address) || '';
+        return `${address}`;
+    },
+    FileMainDocument(state): string {
+        const { sportsman } = state;
+        const file_main_document = (sportsman && sportsman.file_main_document) || '';
+        return `${file_main_document}`;
+    },
+    Rusada(state): string {
+        const { sportsman } = state;
+        const rusada = (sportsman && sportsman.rusada) || '';
+        return `${rusada}`;
+    },
+    Rank(state): string {
+        const { sportsman } = state;
+        const rank = (sportsman && sportsman.rank) || '';
+        return `${rank}`;
+    },
+    DuanCzi(state): string {
+        const { sportsman } = state;
+        const duan_czi = (sportsman && sportsman.duan_czi) || '';
+        return `${duan_czi}`;
+    },
+    RegionInfo(state): string {
+        const { sportsman } = state;
+        const city = (sportsman && sportsman.city?.name_of_city) || '';
+        const name_of_region = (sportsman && sportsman.city?.name_of_region?.name_of_region) || '';
+
+        return `${city} ${name_of_region}`;
+    },
 };
--- client/wushusystemclient/src/views/CardSportsman.vue
+++ client/wushusystemclient/src/views/CardSportsman.vue
@@ -1,6 +1,74 @@
 <template>
     <main>
-        {{ fullName }}
+        <div class="container py-4">
+            <header class="pb-3 mb-4 border-bottom">
+                <span class="fs-4"
+                    >Информация о спортсмене:
+                    <br />
+                    <strong>
+                        {{ fullName }}
+                    </strong>
+                </span>
+            </header>
+            <div class="p-5 mb-4 bg-light rounded-3">
+                <div class="card-group">
+                    <div>
+                        <img
+                            class="card-img-top"
+                            style="width: 15rem"
+                            :src="SportsmanPhoto"
+                            alt="Card image cap"
+                        />
+                    </div>
+                    <div class="card mb-3" style="width: 18rem">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                ФИО: <strong>{{ fullName }}</strong>
+                            </li>
+                            <li class="list-group-item">
+                                Пол: <strong>{{ Gender }}</strong>
+                            </li>
+                            <li class="list-group-item">
+                                Дата рождения: <strong>{{ DateOfBirth }}</strong>
+                            </li>
+                            <li class="list-group-item">
+                                Разряд: <strong>{{ Rank }}</strong>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                Адрес прописки: <strong>{{ Address }}</strong>
+                            </li>
+                            <li class="list-group-item">
+                                Фотография паспорта:
+                                <a :href="SportsmanPhoto" class="card-link">Просмотр</a>
+                            </li>
+                            <li class="list-group-item">
+                                РУСАДА:
+                                <a :href="Rusada" class="card-link">Просмотр</a>
+                            </li>
+                            <li class="list-group-item">
+                                Дуань/Цзи: <strong>{{ DuanCzi }}</strong>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Информация о регионе</h5>
+                        <ul>
+                            <li class="list-group-item">
+                                Город: <strong>{{ RegionInfo }}</strong>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -21,6 +89,24 @@ export default class Sportsman extends Vue {
     getSportsman: any;
     @Getter('fullName', { namespace })
     fullName: string | undefined;
+    @Getter('Gender', { namespace })
+    Gender: string | undefined;
+    @Getter('SportsmanPhoto', { namespace })
+    SportsmanPhoto: string | undefined;
+    @Getter('DateOfBirth', { namespace })
+    DateOfBirth: string | undefined;
+    @Getter('Address', { namespace })
+    Address: string | undefined;
+    @Getter('FileMainDocument', { namespace })
+    FileMainDocument: string | undefined;
+    @Getter('Rusada', { namespace })
+    Rusada: string | undefined;
+    @Getter('Rank', { namespace })
+    Rank: string | undefined;
+    @Getter('DuanCzi', { namespace })
+    DuanCzi: string | undefined;
+    @Getter('RegionInfo', { namespace })
+    RegionInfo: string | undefined;
 
     mounted(): void {
         this.getSportsman(this.$route.params.id);
@@ -28,4 +114,8 @@ export default class Sportsman extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.card-cont {
+    padding-top: 15px;
+}
+</style>
