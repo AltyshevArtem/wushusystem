9ab64e0933e706f18b056ec79a2fdd304bd9853c
altyshevartem <altyshev2013@mail.ru>
2021-07-14 00:30:31 +0500

    страница 404

---
+++ client/wushusystemclient/src/components/NotFound/PageNotFound.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-template">
+                    <p style="font-size: 150px">404</p>
+                    <h2>Not Found</h2>
+                    <div class="error-details">
+                        Извините, запрашиваемая страница не найдена или удалена!
+                    </div>
+                    <div class="error-actions">
+                        <a href="/" class="btn btn-primary btn-lg"
+                            ><span class="glyphicon glyphicon-home"></span> На Главную
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'PageNotFound',
+};
+</script>
+
+<style>
+.error-template {
+    padding: 40px 15px;
+    text-align: center;
+}
+.error-actions {
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+.error-actions .btn {
+    margin-right: 10px;
+}
+</style>
