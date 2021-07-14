1bd923d750fd968267bd4da48b900786b17f92b9
Artem <62567047+AltyshevArtem@users.noreply.github.com>
2021-07-14 23:19:15 +0500

    Not found page (#41)
    * not found
    
    * страница 404
    
    * исправление замечений

--- client/wushusystemclient/src/router/index.ts
+++ client/wushusystemclient/src/router/index.ts
@@ -12,6 +12,12 @@ const routes: Array<RouteRecordRaw> = [
         name: 'Sportsman',
         component: () => import('../views/CardSportsman.vue'),
     },
+    {
+        //TODO: Сделать так, чтобы на домашней странице не было 404
+        path: '/:NotFound(.*)*',
+        name: 'NotFound',
+        component: () => import('../components/NotFound/PageNotFound.vue'),
+    },
 ];
 
 const router = createRouter({
