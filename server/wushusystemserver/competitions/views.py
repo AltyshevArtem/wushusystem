2e408a87c14eec905ec671a57290172deabad411
duutka <loj7mlbdf6l9@gmail.com>
2022-01-30 10:10:52 +0500

    serializer, view, url

--- server/wushusystemserver/competitions/views.py
+++ server/wushusystemserver/competitions/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from competitions.serializers import *
+from competitions.models import *
+from rest_framework import viewsets
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerialize
+
+class DisciplineViewSet(viewsets.ModelViewSet):
+    queryset = Discipline.objects.all()
+    serializer_class = DisciplineSerialize
+
+class CompetitonViewSet(viewsets.ModelViewSet):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitonSerialize
+
+class CompetitionGroupViewSet(viewsets.ModelViewSet):
+    queryset = CompetitionGroup.objects.all()
+    serializer_class = CompetitionGroupSerialize
